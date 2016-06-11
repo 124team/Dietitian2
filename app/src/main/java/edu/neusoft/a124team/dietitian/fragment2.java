4238e127444923f2d85819a118d0fea60d4c0f55
Sweet-dream <992308987@qq.com>
2016-06-11 22:01:02 +0800

    增加定制图片切换

--- app/src/main/java/edu/neusoft/a124team/dietitian/fragment2.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/fragment2.java
@@ -2,6 +2,8 @@ package edu.neusoft.a124team.dietitian;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import edu.neusoft.a124team.dietitian.yuJiangPeng.Y_detail_data;
 
 
@@ -17,13 +22,38 @@ public class fragment2 extends Fragment {
     private ImageView headImage1,headImage2,headImage3,headImage4,headImage5,headImage6,
             headImage7, headImage8;
     private ScrollView mScrollView;
- //   private Toolbar mtoolbar;
 
+ //   private Toolbar mtoolbar;
+      int []images= {R.drawable.y_a,R.drawable.y_b,R.drawable.y_c,R.drawable.y_d,R.drawable.y_e};
+     int Y_SIGN=17; //传递标志
+      int num=0;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout2, container, false);
        // mtoolbar=(Toolbar)getActivity().findViewById(R.id.y_major_toolbar);
+        final ImageView imageView;
+        imageView= (ImageView)view.findViewById(R.id.img_nutrition);
+      final Handler handler =new Handler(){
+          @Override
+          public void handleMessage(Message msg) {
+              super.handleMessage(msg);
+              if(msg.what==Y_SIGN){
+                  imageView.setImageResource(images[num++]);
+                  if(num>=images.length){
+                      num=0;
+                  }
+              }
+          }
+      };
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message msg =new Message();
+                msg.what=Y_SIGN;
+                handler.sendMessage(msg);
+            }
+        }, 3000,3000);
 
         mScrollView=(ScrollView)view.findViewById(R.id.y_scrollView_consult);
         mScrollView.post(new Runnable() {
