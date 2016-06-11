c2f2d00d839cbc9303e9b957929669591ecca32c
Sweet-dream <992308987@qq.com>
2016-06-11 18:06:01 +0800

    增加付款方式弹窗------>0.0

---
+++ app/src/main/java/edu/neusoft/a124team/dietitian/wangRui/S_loginClearEdit.java
@@ -0,0 +1,99 @@
+package edu.neusoft.a124team.dietitian.wangRui;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.EditText;
+
+/**
+ * Created by wang5 on 2016/6/11.
+ */
+public class S_loginClearEdit extends EditText {
+    private final String TAG = "S_loginClearEdit";
+    private Drawable dRight;
+    private Rect rBounds;
+
+    public S_loginClearEdit(Context paramContext) {
+        super(paramContext);
+        initEditText();
+    }
+
+    public S_loginClearEdit(Context paramContext, AttributeSet paramAttributeSet) {
+        super(paramContext, paramAttributeSet);
+        initEditText();
+    }
+
+    public S_loginClearEdit(Context paramContext, AttributeSet paramAttributeSet, int paramInt) {
+        super(paramContext, paramAttributeSet, paramInt);
+        initEditText();
+    }
+
+    // 初始化edittext 控件
+    private void initEditText() {
+        setEditTextDrawable();
+        addTextChangedListener(new TextWatcher() { // 对文本内容改变进行监听
+            @Override
+            public void afterTextChanged(Editable paramEditable) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence paramCharSequence, int paramInt1, int paramInt2, int paramInt3) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence paramCharSequence, int paramInt1, int paramInt2, int paramInt3) {
+                S_loginClearEdit.this.setEditTextDrawable();
+            }
+        });
+    }
+
+    // 控制图片的显示
+    public void setEditTextDrawable() {
+        if (getText().toString().length() == 0) {
+            setCompoundDrawables(null, null, null, null);
+        } else {
+            setCompoundDrawables(null, null, this.dRight, null);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        this.dRight = null;
+        this.rBounds = null;
+
+    }
+
+    /**
+     * 添加触摸事件 点击之后 出现 清空editText的效果
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent paramMotionEvent) {
+        if ((this.dRight != null) && (paramMotionEvent.getAction() == 1)) {
+            this.rBounds = this.dRight.getBounds();
+            int i = (int) paramMotionEvent.getRawX();// 距离屏幕的距离
+            // int i = (int) paramMotionEvent.getX();//距离边框的距离
+            if (i > getRight() - 3 * this.rBounds.width()) {
+                setText("");
+                paramMotionEvent.setAction(MotionEvent.ACTION_CANCEL);
+            }
+        }
+        return super.onTouchEvent(paramMotionEvent);
+    }
+
+    /**
+     * 显示右侧X图片的
+     *
+     * 左上右下
+     */
+    @Override
+    public void setCompoundDrawables(Drawable paramDrawable1, Drawable paramDrawable2, Drawable paramDrawable3, Drawable paramDrawable4) {
+        if (paramDrawable3 != null)
+            this.dRight = paramDrawable3;
+        super.setCompoundDrawables(paramDrawable1, paramDrawable2, paramDrawable3, paramDrawable4);
+    }
+}
---
+++ app/src/main/java/edu/neusoft/a124team/dietitian/yuJiangPeng/Y_popups_activity.java
@@ -0,0 +1,66 @@
+package edu.neusoft.a124team.dietitian.yuJiangPeng;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import edu.neusoft.a124team.dietitian.R;
+
+public class Y_popups_activity extends AppCompatActivity implements View.OnClickListener {
+   private LinearLayout mlinearyout;
+    private RelativeLayout r1,r2,r3;
+    private Button concel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.MyDialogStyleBottom);
+        setContentView(R.layout.activity_y_popups);
+        //设置与手机边框无距离
+        getWindow().setLayout(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        mlinearyout=(LinearLayout)this. findViewById(R.id.y_popups_layout);
+        r1=(RelativeLayout)this. findViewById(R.id.y_r1);
+        r2=(RelativeLayout)this. findViewById(R.id.y_r2);
+        r3=(RelativeLayout)this. findViewById(R.id.y_r3);
+        concel=(Button) findViewById(R.id.y_popups_button);
+        //添加选择窗口范围监听可以优先获取触点，即不再执行onTouchEvent()函数，
+        // 点击其他地方时执行onTouchEvent()函数销毁Activity
+        mlinearyout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击窗口外部关闭窗口！",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        r1.setOnClickListener(this);
+        r2.setOnClickListener(this);
+        r3.setOnClickListener(this);
+        concel.setOnClickListener(this);
+    }
+    //实现onTouchEvent触屏函数但点击屏幕时销毁本Activity
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        finish();
+        return true;
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.y_r1:
+                break;
+            case R.id.y_r2:
+                break;
+            case R.id.y_r3:
+                break;
+            case R.id.y_popups_button:
+                break;
+            default:
+                break;
+        }
+        finish();
+    }
+}
