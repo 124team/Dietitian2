8eb9ba494231d5d9fd012477773f1b2a02df11dd
wanghan <415471768@qq.com>
2016-06-14 07:52:45 +0800

    增加：添加了App图标，但是有异常

--- app/src/main/java/edu/neusoft/a124team/dietitian/wangHan/activityAdapter/W_activity_adapterActivity.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/wangHan/activityAdapter/W_activity_adapterActivity.java
@@ -1,10 +1,13 @@
 package edu.neusoft.a124team.dietitian.wangHan.activityAdapter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import edu.neusoft.a124team.dietitian.R;
+import edu.neusoft.a124team.dietitian.wangHan.W_camera;
 
 public class W_activity_adapterActivity extends AppCompatActivity {
 
@@ -15,15 +18,16 @@ public class W_activity_adapterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_w_activity_adapter);
 
-     //   camera = (LinearLayout) findViewById(R.id.w_btn_adapter_camera);
- /*     camera.setOnClickListener(new View.OnClickListener() {
+      camera = (LinearLayout) findViewById(R.id.w_btn_adapter_camera);
+      camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(W_activity_adapterActivity.this, W_CamreaActivity.class);
-                startActivity(intent);*//*
-            }
-        });*/
+                /*Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);*/
+                Intent intent = new Intent(W_activity_adapterActivity.this, W_camera.class);
+                startActivity(intent);
 
+            }
+        });
 
 
     }
