ab4dfa3cf56e02f1ff5bd1849ffe24bbe9177850
haodengke <471440364@qq.com>
2016-09-05 20:27:44 +0800

    增加图片网络编程2

--- app/src/main/java/edu/neusoft/a124team/dietitian/haoDengKe/H_candy.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/haoDengKe/H_candy.java
@@ -1,14 +1,24 @@
 package edu.neusoft.a124team.dietitian.haoDengKe;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import edu.neusoft.a124team.dietitian.R;
 import edu.neusoft.a124team.dietitian.haoDengKe.utils.NetUtils;
 
 public class H_candy extends AppCompatActivity {
-
+    private Handler pic_hdl;
+    private ImageView hImageViewCandy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +27,56 @@ public class H_candy extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.activity_h_candy);
+        hImageViewCandy=(ImageView)findViewById(R.id.h_imagebutton_fruit_select);
+        pic_hdl = new PicHandler();
+        Thread t = new LoadPicThread();
+        t.start();
+
 
     }
+    class LoadPicThread extends Thread {
+
+        @Override
+        public void run() {
+            Bitmap img = getUrlImage("http://139.129.57.32:8080/My12306/Food/h_imagebutton_fruit_select.jpg");
+
+            Message msg= pic_hdl.obtainMessage();
+
+            msg.what = 0;
+
+            msg.obj = img;
+
+            pic_hdl.sendMessage(msg);
+
+        }
+    }
+    class PicHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            //String s = (String)msg.obj;
+            //ptv.setText(s);
+            Bitmap myimg = (Bitmap) msg.obj;
+            hImageViewCandy.setImageBitmap(myimg);
+        }
+    }
+    public Bitmap getUrlImage(String url) {
+        Bitmap img = null;
+        try {
+            URL picurl = new URL(url);
+            // 获得连接
+            HttpURLConnection conn = (HttpURLConnection)picurl.openConnection();
+            conn.setConnectTimeout(6000);//设置超时
+            conn.setDoInput(true);
+            conn.setUseCaches(false);//不缓存
+            conn.connect();
+            InputStream is = conn.getInputStream();//获得图片的数据流
+            img = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+
 }
--- app/src/main/java/edu/neusoft/a124team/dietitian/haoDengKe/H_fash.java
+++ app/src/main/java/edu/neusoft/a124team/dietitian/haoDengKe/H_fash.java
@@ -1,14 +1,24 @@
 package edu.neusoft.a124team.dietitian.haoDengKe;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import edu.neusoft.a124team.dietitian.R;
 import edu.neusoft.a124team.dietitian.haoDengKe.utils.NetUtils;
 
 public class H_fash extends AppCompatActivity {
-
+    private Handler pic_hdl;
+    private ImageView hImageViewFash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +27,55 @@ public class H_fash extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.activity_h_fash);
+        hImageViewFash=(ImageView)findViewById(R.id.hImageViewFash);
+        pic_hdl = new PicHandler();
+        Thread t = new LoadPicThread();
+        t.start();
+    }
+
+
+    class LoadPicThread extends Thread {
+
+        @Override
+        public void run() {
+            Bitmap img = getUrlImage("http://139.129.57.32:8080/My12306/Food/h_imagebutton_select2s.jpg");
+
+            Message msg= pic_hdl.obtainMessage();
+
+            msg.what = 0;
+
+            msg.obj = img;
+
+            pic_hdl.sendMessage(msg);
+
+        }
+    }
+    class PicHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            //String s = (String)msg.obj;
+            //ptv.setText(s);
+            Bitmap myimg = (Bitmap) msg.obj;
+            hImageViewFash.setImageBitmap(myimg);
+        }
+    }
+    public Bitmap getUrlImage(String url) {
+        Bitmap img = null;
+        try {
+            URL picurl = new URL(url);
+            // 获得连接
+            HttpURLConnection conn = (HttpURLConnection)picurl.openConnection();
+            conn.setConnectTimeout(6000);//设置超时
+            conn.setDoInput(true);
+            conn.setUseCaches(false);//不缓存
+            conn.connect();
+            InputStream is = conn.getInputStream();//获得图片的数据流
+            img = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return img;
     }
 }
